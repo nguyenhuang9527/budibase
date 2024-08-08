03729f1f9bbd9b29695cdad2dc5809e6c7da3133
Adria Navarro <adria@budibase.com>
2024-08-08 10:48:14 +0200

    Extra tests

--- packages/server/src/sdk/app/views/tests/utils.spec.ts
+++ packages/server/src/sdk/app/views/tests/utils.spec.ts
@@ -1,27 +1,63 @@
-import { generator } from "@budibase/backend-core/tests"
 import { SearchFilters } from "@budibase/types"
 import { removeInvalidFilters } from "../utils"
 
 describe("view utils", () => {
   describe("removeInvalidFilters", () => {
+    const fullFilters: SearchFilters = {
+      equal: { one: "foo" },
+      $or: {
+        conditions: [
+          {
+            equal: { one: "foo2", two: "bar" },
+            notEmpty: { one: null },
+            $and: {
+              conditions: [
+                {
+                  equal: { three: "baz" },
+                  notEmpty: { forth: null },
+                },
+              ],
+            },
+          },
+        ],
+      },
+      $and: {
+        conditions: [{ equal: { one: "foo2" }, notEmpty: { one: null } }],
+      },
+    }
     it("can filter empty queries", () => {
       const filters: SearchFilters = {}
       const result = removeInvalidFilters(filters, [])
       expect(result).toEqual({})
     })
 
-    it("don't trim any valid field", () => {
-      const filters: SearchFilters = {
-        equal: { one: generator.word() },
+    it("does not trim any valid field", () => {
+      const result = removeInvalidFilters(fullFilters, [
+        "one",
+        "two",
+        "three",
+        "forth",
+      ])
+      expect(result).toEqual(fullFilters)
+    })
+
+    it("trims invalid field", () => {
+      const result = removeInvalidFilters(fullFilters, [
+        "one",
+        "three",
+        "forth",
+      ])
+      expect(result).toEqual({
+        equal: { one: "foo" },
         $or: {
           conditions: [
             {
-              equal: { one: generator.word(), two: generator.word() },
+              equal: { one: "foo2" },
               notEmpty: { one: null },
               $and: {
                 conditions: [
                   {
-                    equal: { three: generator.word() },
+                    equal: { three: "baz" },
                     notEmpty: { forth: null },
                   },
                 ],
@@ -30,18 +66,9 @@ describe("view utils", () => {
           ],
         },
         $and: {
-          conditions: [
-            { equal: { one: generator.word() }, notEmpty: { one: null } },
-          ],
+          conditions: [{ equal: { one: "foo2" }, notEmpty: { one: null } }],
         },
-      }
-      const result = removeInvalidFilters(filters, [
-        "one",
-        "two",
-        "three",
-        "forth",
-      ])
-      expect(result).toEqual(filters)
+      })
     })
   })
 })
